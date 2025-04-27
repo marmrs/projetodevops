f7bfdc1aa4e1dda548ddbd6f20b33f23631670b6
Maren M. <127352355+marmrs@users.noreply.github.com>
2025-04-27 18:11:33 -0300

    teste unitarios

---
+++ projetosoa/src/test/java/projetosoa/TestesUnitarios.java
@@ -0,0 +1,34 @@
+package projetosoa;
+import org.junit.Test;
+import soap.Calculadora;
+import static org.junit.Assert.assertEquals;
+
+public class TestesUnitarios {
+@Test	
+public void testesomar() {
+	Calculadora calculadora = new Calculadora();
+    int resultado = calculadora.somar(2, 3);
+    assertEquals(5, resultado);	
+}
+
+@Test	
+public void testesubstrair() {
+	Calculadora calculadora = new Calculadora();
+    int resultado = calculadora.subtrair(3, 2);
+    assertEquals(1, resultado);	
+}
+
+@Test	
+public void testedividir() {
+	Calculadora calculadora = new Calculadora();
+    int resultado = calculadora.dividir(10, 5);
+    assertEquals(2, resultado);	
+}
+
+@Test	
+public void testemultiplicar() {
+	Calculadora calculadora = new Calculadora();
+    int resultado = calculadora.dividir(10, 5);
+    assertEquals(50, resultado);	
+}
+}
